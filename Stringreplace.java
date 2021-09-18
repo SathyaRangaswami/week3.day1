7b5649d2be0e649671236453c250a1939b3b2ab1
SathyaRangaswami <90094497+SathyaRangaswami@users.noreply.github.com>
2021-09-18 18:28:49 +0530

    Add files via upload

--- Stringreplace.java
+++ Stringreplace.java
@@ -8,21 +8,7 @@ public class Stringreplace {
 		  System.out.println( replacedword.concat("#"));
 		  System.out.println(replacedword.substring(5,14));
 
-		/*String str = "I am working with Java8";
-		String str1 = str.replaceAll("\\s", "");
-		String replace = "";
-		// replace 8 to 11
-		// Print the characters from 5 to 14
-		char[] charArray = str1.toCharArray();
-			 replace = str1.replace(charArray[8], charArray[11]);
-		
-		System.out.println(replace);
-
-		System.out.print("String print from 5 to 14 :");
-		for (int j = 5; j <= 14; j++) {
-			System.out.print(charArray[j]);
-		}*/
-		
+				
 	}
 
 }
